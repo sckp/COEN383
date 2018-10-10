85051d41a2333461b9c6610a1a89c68c1bf180df
Sarah Pagnani <spagnani@scu.edu>
2018-10-09 17:52:59 -0700

    Update P2/HPF.c

--- P2/HPF.c
+++ P2/HPF.c
@@ -85,33 +85,22 @@ void processJobs_nonHPF(CPU* cpu, Queue* queue, Job* jobs, Job* completed, int n
     
     cpu->job = malloc(sizeof(Job));
     
-    // loop through all of the jobs, each loops represents a quantum of time
-    // Metrics for the loop are if the cpu clock is under 100
-    // there is a job on the cpu
-    // or if there is a job in any of the queues
     while((100 > clock_cpu_HPF) || (!cpu->available) || (!isEmpty(&queue[0])) || (!isEmpty(&queue[1]))
           || (!isEmpty(&queue[2])) || (!isEmpty(&queue[3]))) {
-        // check if the cpu clock has reached 100
         if(100 == clock_cpu_HPF) {
-            // remove all jobs from all queues that have not started yet
+            //PAST TIME LIMIT, REMOVE JOBS
             for(int i = 0; i < 4; i++) {
                 removeJobFromQueue_nonHPF(&queue[i], completed);
             }
         }
-        // check if a job has arrive and if it should be place into the queue
-        // use a while loop to add all jobs that are supposed to arrive
-        // incase multiple arrive at the same time
+        //ADD JOBS TO RIGHT PRIORITY QUEUE
         while((clock_cpu_HPF == jobs[indexJob].arrival_time) && (numJobs > indexJob)){
-            // place the job into its respective job queue
             push(&queue[(jobs[indexJob].priority - 1)], jobs[indexJob]);
-            // increment the jobIndex
             indexJob++;
         }
         
-        // check if the CPU is not available
-        if(!cpu->available && 1 > jobs[indexJob].remaining_service_time) {  // CPU is not available
-            // job has run for its quanta of time so remove it from the CPU
-            // check which queue the job on the CPU is supposed to be placed back into
+        //CHECK JOB IS RUNNING AND IT IS FINISHED
+        if(!cpu->available) {
             if(1 == cpu->job->priority) {
                 removeFromCPU_nonHPF(cpu, queue, completed, 0);
             }
@@ -124,39 +113,26 @@ void processJobs_nonHPF(CPU* cpu, Queue* queue, Job* jobs, Job* completed, int n
             else if(4 == cpu->job->priority) {
                 removeFromCPU_nonHPF(cpu, queue, completed, 3);
             }
-        } else if (!cpu->available && 1 < jobs[indexJob].remaining_service_time) {
-            jobs[indexJob].remaining_service_time -= 1;
         }
         
-        // check if there is a job on the CPU
-        if(cpu->available) { // CPU is available
-            // check if there is a job in the highest priority queue
+        //NO JOB RUNNING
+        if(cpu->available) {
             if(!isEmpty(&queue[0])) {
-                // move the job from the queue to the CPU
                 moveToCPU_nonHPF(cpu, &queue[0]);
             }
-            // else check the next priority queue
             else if(!isEmpty(&queue[1])) {
-                // move the job from the queue to the CPU
                 moveToCPU_nonHPF(cpu, &queue[1]);
             }
-            // else check the next priority queue
             else if(!isEmpty(&queue[2])) {
-                // move the job from the queue to the CPU
                 moveToCPU_nonHPF(cpu, &queue[2]);
             }
-            // else check the next priority queue
             else if(!isEmpty(&queue[3])) {
-                // move the job from the queue to the CPU
                 moveToCPU_nonHPF(cpu, &queue[3]);
             }
-            // otherwise the CPU remains idle
         }
-        
-        // increment the global clock
         clock_cpu_HPF++;
     }
-    // free the memory in the CPU struct
+    //FREE DYNAMIC MEMORY
     free(cpu->job);
 }
 
@@ -168,18 +144,17 @@ void moveToCPU_nonHPF(CPU* c, Queue* q) {
     c->job->priority = q->head->job.priority;
     c->job->remaining_service_time = q->head->job.remaining_service_time;
     // check if this is the first time the job is using the CPU
-    if(1 > q->head->job.start_time) {
-        c->job->start_time = clock_cpu_HPF;
-    }
-    else {
-        c->job->start_time = q->head->job.start_time;
-    }
+
+    c->job->start_time = clock_cpu_HPF;
+    
     c->job->finish_time = q->head->job.finish_time;
-    // set the age of the process and return it to zero when it gets the CPU
+
     c->job->age = 0;
+    
     // remove the job from the queue
     pop(q);
-    // set the cpu to being available
+   
+    //CPU BUSY
     c->available = false;
 }
 
@@ -211,53 +186,9 @@ void removeFromCPU_nonHPF(CPU* c, Queue* q, Job* complete, int queueIndex) {
         complete[indexFinished].finish_time = clock_cpu_HPF;
         // increment the finished index counter
         indexFinished++;
+        c->available = true;
+
     }
-    // else the job needs to go back in the queue
-    else {
-        // check all the queues with regards to their priority level and see if there is a higher
-        // priority job waiting
-        if(0 == queueIndex) {
-            // This job is a high priority job see if there are other high priorities waiting
-            if(!isEmpty(&q[0])) {
-                push(&q[queueIndex], *c->job);
-            }
-            else {
-                return;
-            }
-        }
-        // check next highest priority queue
-        else if(1 == queueIndex) {
-            // check if there is a higher priority queue with a job or a job with the same priority
-            if((!isEmpty(&q[0])) || (!isEmpty(&q[1]))) {
-                push(&q[queueIndex], *c->job);
-            }
-            else {
-                return;
-            }
-        }
-        // check 2nd lowest priority queue
-        else if(2 == queueIndex) {
-            // check if there is a higher priority queue with a job or a job with the same priority
-            if((!isEmpty(&q[0])) || (!isEmpty(&q[1])) || (!isEmpty(&q[2]))) {
-                push(&q[queueIndex], *c->job);
-            }
-            else {
-                return;
-            }
-        }
-        // check lowest priority queue
-        else if(3 == queueIndex) {
-            // check if there is a higher priority queue with a job or a job with the same priority
-            if((!isEmpty(&q[0])) || (!isEmpty(&q[1])) || (!isEmpty(&q[2])) || (!isEmpty(&q[3]))) {
-                push(&q[queueIndex], *c->job);
-            }
-            else {
-                return;
-            }
-        }
-    }
-    // set the CPU to being free
-    c->available = true;
 }
 
 // this function removes jobs from the queue that haven't start

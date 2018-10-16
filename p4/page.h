3e049fb74df3b56fa4d30e24a395efe02f14407d
sckp <32909879+sckp@users.noreply.github.com>
2018-10-16 10:39:39 -0700

    Create page.h

---
+++ p4/page.h
@@ -0,0 +1,32 @@
+#ifndef PAGE_H
+#define PAGE_H
+
+int clock;
+
+typedef struct {
+  int pid;
+  int page_num;
+  struct page* next;
+  
+  int first_timestamp; //first time in memeory
+  int last_timestamp; //last time used
+} page;
+
+typedef struct {
+  page *head;
+} list;
+
+typdef struct {
+  int pid;
+  int size; //number of pages
+  int arrival_time;
+  int service_duration;
+} job;
+
+void FCFS(list *);
+void LRU(list *);
+void LFU(list *);
+void MFU(list *);
+void RAN(list *);
+
+#endif

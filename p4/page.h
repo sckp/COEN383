6d691a11614d1b8af381d2ba357d1d6753ee5d8c
sckp <32909879+sckp@users.noreply.github.com>
2018-10-16 19:05:59 -0700

    Update page.h

--- p4/page.h
+++ p4/page.h
@@ -11,23 +11,27 @@ typedef struct {
   int LFU_count; //count for LFU and MFU
   int first_timestamp; //first time in memeory
   int last_timestamp; //last time used
-} page;
+} Page;
 
 typedef struct {
   page *head;
-} list;
+} List;
 
 typdef struct {
   int pid;
   int size; //number of pages
   int arrival_time;
   int service_duration;
-} job;
+} Job;
 
-void FCFS(list *);
-void LRU(list *);
-void LFU(list *);
-void MFU(list *);
-void RAN(list *);
+void FCFS(List *); //first come first serve
+void LRU(List *); //least recently used
+void LFU(List *); //least frequently used
+void MFU(List *); //most frequently used
+void RAN(List *); //random
+
+void create_list(List *); //initialize page list
+void print_list(List *); //print page list
+void print_job(Job * ); //print job queue
 
 #endif

a8949910eb2f30436ea2681494142bf683ef7ac3
sckp <32909879+sckp@users.noreply.github.com>
2018-10-16 19:06:24 -0700

    Update FCFS.c

--- p4/FCFS.c
+++ p4/FCFS.c
@@ -1,8 +1,8 @@
 #include "page.h"
 
-void FCFS (page * page_list) {
-  page * temp = page_list ->head;
-  page * evict = temp;
+void FCFS (Page * page_list) {
+  Page * temp = page_list ->head;
+  Page * evict = temp;
   while(temp) {
     if (temp->arrival_time < evict->arrival_time) {
       evict = temp;
